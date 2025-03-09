e55ac5df06bc22c074d2574f3c25aaf8e6935ad4
Diego Pitsillidis <diego.pitsillides@gmail.com>
2025-03-09 19:14:58 +0200

    onclick and onLevel change handler

--- src/modules/GameWithHooks/GameWithHooks.tsx
+++ src/modules/GameWithHooks/GameWithHooks.tsx
@@ -2,21 +2,38 @@ import React, { FC, useState } from 'react' ;
 
 import { GameLevels, LevelNames, GameSettings } from '@/modules/GameSettings';
 
-import { Field, emptyFieldGenerator, CellState } from '@/helpers/Field';
+import { Field, emptyFieldGenerator, CellState, fieldGenerator, Coords } from '@/helpers/Field';
 import { Top } from '@/components/Top';
 import { Scoreboard } from '@/components/Scoreboard';
 import { Grid } from '@/components/Grid/Grid';
 import { GameArea, Wrapper, GameOver } from '@/components/Game';
+import { openCell } from '@/helpers/CellsManipulator';
 
 export const GameWithHooks: FC = () => {
     const [level, setLevel] = useState<LevelNames>('beginner');
 
     const [size, bombs] = GameSettings[level];
 
-    const test: Field = [];
-    console.log(bombs, test);
+    const [playerField, setPlayerField] = useState<Field>(
+        emptyFieldGenerator(size, CellState.hidden)
+    );
+    
+    const gameField = fieldGenerator(size, bombs / (size * size));
 
-    const playerField = emptyFieldGenerator(size, CellState.hidden);
+    const onClick = (coords: Coords)=> {
+        const newPlayerField = openCell(coords, playerField, gameField);
+        setPlayerField([...newPlayerField]);
+    }
+
+    const onChangeLevel = ({
+        target: { value: level },
+    }: React.ChangeEvent<HTMLSelectElement>) => {
+        setLevel(level as LevelNames);
+        const [size] = GameSettings[level as LevelNames];
+
+        const newPlayerField = emptyFieldGenerator(size, CellState.hidden);
+        setPlayerField([...newPlayerField]);
+    };
 
     return (
         <Wrapper>
@@ -29,13 +46,11 @@ export const GameWithHooks: FC = () => {
                     bombs='10'
                     levels={GameLevels}
                     defaultLevel={level}
-                    onChangeLevel={({ target: { value } }) => 
-                        setLevel(value as LevelNames)
-                    }
+                    onChangeLevel={onChangeLevel}
                     onReset={() => null}
                 />
                 <GameOver onClick={() => null} isWin={true} />
-                <Grid onClick={() => null} onContextMenu={() => null}>
+                <Grid onClick={onClick} onContextMenu={() => null}>
                     {playerField}
                 </Grid>
             </GameArea>
