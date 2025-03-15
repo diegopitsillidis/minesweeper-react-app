b5a30fb52f6847b1dbbaa99b38ad8daec0af20fb
Diego Pitsillidis <diego.pitsillides@gmail.com>
2025-03-15 20:45:09 +0200

    clean up

--- src/index.tsx
+++ src/index.tsx
@@ -7,35 +7,3 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <GameWithHooks />
 );
-
-{/* const Game: FC = () => (
-    <Wrapper>
-        <Top feature="Flag" firstAction="right click">
-            Minesweeper
-        </Top>
-    </Wrapper>
-);
-
-
-ReactDOM.render(<Game>{StaticField}</Game>, document.getElementById('root')); */}
-
-// import { Top } from './components/Top/Top';
-// import {Scoreboard} from './components/Scoreboard'
-
-// //test change
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-// root.render(
-//     <React.StrictMode>
-//         <>
-//             <Top feature='Flag' firstAction='Ctrl' secondAction='Click'>
-//                 Minesweeper
-//             </Top>
-//             <Scoreboard 
-//                 time="000"
-//                 levels={['beginner', 'intermediate', 'expert']}
-//                 mines="010"
-//                 onReset={() => null}
-//             />
-//         </>
-//     </React.StrictMode>
-// );
